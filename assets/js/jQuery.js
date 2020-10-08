8a229936d0352f3f190c12fa3a60b436158def07
shan-ali-brainx <shan.ali@brainxtech.com>
2020-10-08 15:12:55 +0500

    Error fixing

--- assets/js/jQuery.js
+++ assets/js/jQuery.js
@@ -12,12 +12,3 @@ function preview( uploader ) {
 $("#imageUpload").change(function(){
     preview( this );
 });
-
-function IsEmail(email) {
-    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if(!regex.test(email)) {
-       return false;
-    }else{
-       return true;
-    }
-}
