7923a3efbee8127aafa23e222184c3e5425da478
shan-ali-brainx <shan.ali@brainxtech.com>
2020-10-08 15:06:58 +0500

    Validation-code

--- assets/js/jQuery.js
+++ assets/js/jQuery.js
@@ -13,3 +13,11 @@ $("#imageUpload").change(function(){
     preview( this );
 });
 
+function IsEmail(email) {
+    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if(!regex.test(email)) {
+       return false;
+    }else{
+       return true;
+    }
+}
